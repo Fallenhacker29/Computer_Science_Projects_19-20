798656d9629b05629c2db359b1a3cb3998df6f71
Fallenhacker29 <56514528+Fallenhacker29@users.noreply.github.com>
2020-04-30 10:53:21 +0300

    Add files via upload

---
+++ СР_30.04.pas
@@ -0,0 +1,27 @@
+﻿program aaa;
+// двумерный массив
+{
+организовать двумерный массив 5 на 5
+первый элемент 3, каждый последующий на 2 больше
+
+сумма гл диагонали
+}
+
+var arr:array[1..5,1..5] of integer;
+var x,y,prev,res:integer;
+
+begin
+  res:= 0;
+prev:=1;
+for y:= 1 to 5 do
+  for x:=1 to 5 do
+    begin
+    arr[y,x]:= prev + 2;
+    prev:=arr[y,x];
+    end;
+writeln(arr);
+for x:=1 to 5 do
+    res := res+ arr[x][x];
+writeln(res);
+
+end.
